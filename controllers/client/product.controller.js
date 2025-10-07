7407e0670d78b3e13998ee84df5b75d5250a20cb
Vo Van Quyen <vvq0522@gmail.com>
2025-10-07 12:23:49 +0700

    build the client product list page

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,18 @@
-module.exports.index = (req, res) => {
+const Product = require('../../models/product.model')
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: 'active',
+        deleted: 'false'
+    })
+
+    const newProducts = products.map(item => {
+        item.newPrice = (item.price * (100 - item.discountPercentage) / 100).toFixed(0)
+        return item
+    })
+
     res.render('client/pages/products/index', {
-        pageTitle: 'Trang danh sách sản phẩm'
+        pageTitle: 'Trang danh sách sản phẩm',
+        products: newProducts
     })
 }
