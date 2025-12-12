679146d818c4add0ac0542e86ca036f723ec90d8
thanhduy01929 <thanhduy03112005@gmail.com>
2025-12-12 21:00:43 +0700

    fix logout

--- routes/client/login.route.js
+++ routes/client/login.route.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const loginController = require("../../controllers/client/login.controller");
+const { loginValidates } = require("../../validates/client/login.validates");
 
 // Hiển thị form đăng nhập
 router.get("/", loginController.renderLogin);
 
-// Xử lý đăng nhập
-router.post("/", loginController.handleLogin);
+// Xử lý đăng nhập + validate
+router.post("/", loginValidates, loginController.handleLogin);
 
 // Đăng xuất
 router.get("/logout", loginController.logout);
--- routes/client/register.route.js
+++ routes/client/register.route.js
@@ -1,54 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
-const User = require("../../models/user-client");
+
+const registerController = require("../../controllers/client/register.controller");
+const { registerValidates } = require("../../validates/client/register.validates");
 
 // Hiển thị form đăng ký
-router.get("/", (req, res) => {
-    res.render("client/pages/auth/register", {
-        pageTitle: "Đăng ký tài khoản"
-    });
-});
+router.get("/", registerController.renderRegister);
 
 // Xử lý đăng ký
-router.post("/", async (req, res) => {
-    try {
-        const { fullName, email, password } = req.body;
-
-        if (!fullName || !email || !password) {
-            return res.render("client/pages/auth/register", {
-                pageTitle: "Đăng ký tài khoản",
-                error: "Vui lòng nhập đầy đủ thông tin!"
-            });
-        }
-
-        const exist = await User.findOne({ email });
-        if (exist) {
-            return res.render("client/pages/auth/register", {
-                pageTitle: "Đăng ký tài khoản",
-                error: "Email đã tồn tại!"
-            });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        await User.create({
-            fullName,
-            email,
-            password: hashedPassword
-        });
-
-    
-
-        return res.redirect("/login");
-
-    } catch (error) {
-        console.error(error);
-        return res.render("client/pages/auth/register", {
-            pageTitle: "Đăng ký tài khoản",
-            error: "Lỗi hệ thống!"
-        });
-    }
-});
+router.post("/", registerValidates, registerController.handleRegister);
 
 module.exports = router;
